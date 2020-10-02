3a63cb2752592b85a52e31ec3c6ba88066f6b79b
Iragael38 <ijoly.research@gmail.com>
2020-10-02 15:56:56 +0200

    02/10.1

---
+++ public/Data-pdf/InstallListePackages.R
@@ -0,0 +1,31 @@
+######    LISTE des packages R utilisés dans le cours de DataMining : à installer avec les droits administrateurs
+
+# Packages a priori nouveaux à installer
+install.packages("Ecdat"); install.packages("MASS")
+install.packages(c("ggplot2", "gcookbook"))
+install.packages("gmodels");install.packages("VGAM");
+install.packages("Hmisc"); install.packages("rgl")
+install.packages("lattice") ; install.packages("Rcmdr")
+install.packages("psych") ; install.packages("rJava")
+install.packages("reshape") ;  install.packages("plotrix")
+install.packages("scatterplot3d") ; install.packages("foreign")
+
+install.packages("grDevices") ;  install.packages("maps")
+install.packages("xtable") ; install.packages("stargazer")
+install.packages('knitr', dependencies = TRUE)
+install.packages('rmarkdown', dependencies = TRUE)
+
+install.packages("car"); install.packages(lmtest) ; install.packages("perturb")
+
+install.packages("gdata") ; install.packages("Cpan")
+install.packages("survival") ; install.packages("km.ci")
+install.packages("KMsurv") ; install.packages("nlme")
+install.packages("lavaan") ; install.packages("semPlot")
+install.packages("AICcmodavg") ; install.packages("mlogit")
+install.packages("faraway") ; install.packages("aod")
+install.packages("gmnl") 
+
+
+# Possible que les packages suivants ne soient plus disponibles selon la version de R installée sur les machines.
+install.packages("xlsx");install.packages("xlsReadWrite");
+
